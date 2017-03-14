f9508e4d8afed24376df8544b6906856ed40d146
Alisson Zampietro <alisson.zampietro@gmail.com>
2017-03-14 11:36:51 -0300

    Implementando nome dos vértices e valores das arestas (incompleto)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var defaultPosition = 100;
+var defaultPosition = 150;
 var tamGraph = 80;
 var positionX = defaultPosition;
 var oldY;
@@ -23,6 +23,11 @@ var drawVertex = function(name, objConnections, G) {
 	var ctx = c.getContext("2d");
 	ctx.beginPath();
 	ctx.arc(positionX,positionY,tamGraph,0,2*Math.PI);
+	
+	//especificações para a escrita do nome do vértice
+    ctx.font = "bold 18px serif";
+    ctx.textBaseline = "bottom";
+	ctx.fillText(name, positionX ,positionY);
 
 	// condicional para fazer com que os vértices fiquem lado a lado
 	if(cont % 2 == 0) {
@@ -35,8 +40,12 @@ var drawVertex = function(name, objConnections, G) {
 		positionY = (positionY+180);
 	}
 
+
+
 	//desenha os vértices
 	ctx.stroke();
+
+	ctx.closePath();
 }
 
 var getRandomColor = function() {
@@ -50,25 +59,36 @@ var getRandomColor = function() {
 
 // função que desenha as conexões
 var drawConnection = function(from, to) {
+	var valor = copyConnections[from][to];
+	//atrivui os vértices as variáveis
+	var from = vertexPosition[from];
+	var to = vertexPosition[to]; 
 
 	var c = document.getElementById("graph");
 	var ctx = c.getContext("2d");
 	var color = getRandomColor();
 
-	ctx.beginPath()
+	ctx.beginPath();
 	ctx.strokeStyle = color;
+
+	//especificações para a escrita do nome do vértice
+    ctx.font = "18px serif";
+    ctx.textBaseline = "bottom";
+    linePositionX = (from.positionX + to.positionX) / Math.floor(Math.random() + 2);
+    linePositionY = (from.positionY + to.positionY) / Math.floor(Math.random() + 2);
+	ctx.fillText(valor, linePositionX ,linePositionY);
 
 	ctx.moveTo(from.positionX, from.positionY);
 	ctx.lineTo(to.positionX,to.positionY);
 
 	ctx.stroke();
-	ctx.closePath()
+	ctx.closePath();
 }
 
 var buildConnections = function() {
 	for(from in copyConnections) {
 		for(to in copyConnections[from]) {
-			drawConnection(vertexPosition[from], vertexPosition[to])
+			drawConnection(from, to);
 		}
 	}
 }
