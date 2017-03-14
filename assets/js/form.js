def8e6cc5386e69283fde85a79e713fe7c1674da
Alisson Zampietro <alisson.zampietro@gmail.com>
2017-03-14 13:09:16 -0300

    Refatorando código e implementando form com validações

---
+++ assets/js/form.js
@@ -0,0 +1,68 @@
+var clearFormFields = function() {
+	var campos = document.getElementsByClassName('fom-control');
+	campos[0].innerHTML = "";		
+	campos[1].innerHTML = "";		
+}
+
+var message = function (text, status) {
+	var defaultMessages = {
+		"success": "alert-success",
+		"error": "alert-danger",
+		"warning": "alert-warning",
+	}
+
+	if(defaultMessages[status] == undefined)
+		status = "warning"; 
+
+	var message = document.getElementById("message");
+	message.classList.add(defaultMessages[status]);
+	message.classList.remove("hidden");
+	message.innerHTML = text;
+	setTimeout(function() {
+		message.classList.add("hidden");
+		message.classList.remove(defaultMessages[status]);
+		message.innerHTML = "";
+	}, 3000);
+
+	if(status == "error")
+		throw new Error(text);
+}
+
+var buildValuesVert = function (keyVertex, amountValues) {
+	var obj = {};
+	var values = amountValues.split(";");
+
+	if(values[values.length - 1] == "")
+		values.splice(-1);
+
+	for (value in values) {
+		var item = values[value].split("=");
+		//verifica se algum valor tem o mesmo nome do vértice
+		if(item.length != 2) {
+			message("O campo de valores está inválido, siga o exemplo do placeholder","error");
+		}
+
+		if(item[0].toUpperCase().localeCompare(keyVertex) == 0) {
+			message("Os valores não podem indicar o próprio vértice","error");
+		}
+
+		obj[item[0].toUpperCase()] = item[1];
+	}
+
+	return obj;
+}
+var inserir = function() {
+	var inputs = document.getElementsByClassName('form-control');
+	
+	if(inputs[0].value == "" || inputs[0].value == undefined || inputs[1].value == "" || inputs[1].value == undefined){
+		message("Algum campo está inválido","error");
+	}
+	var key = inputs[0].value.toUpperCase();
+
+	var obj = buildValuesVert(key,inputs[1].value);
+	drawVertex(key, obj, g);
+}
+
+var finalizar = function() {
+	buildConnections();
+}
